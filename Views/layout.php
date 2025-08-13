3c64343c85f3180001de488199b7a0775c94cc9f
alanfelix <alan.felix@dportenis.com.mx>
2025-08-13 14:28:04 -0700

    Add: Agregamos bootstrap al proyecto

--- Views/layout.php
+++ Views/layout.php
@@ -6,6 +6,7 @@
     <title>Evaluación</title>
     <link rel="stylesheet" href="../views/assets/layout/css/index.css">
     <script src="../views/assets/layout/js/index.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
     <header>
