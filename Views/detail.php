ae781581326f099987ed6cd87883af4dd4b0e410
alanfelix <alan.felix@dportenis.com.mx>
2025-08-14 19:10:21 -0700

    Fix: Corregimos estilos en el detalle de Menú

--- Views/detail.php
+++ Views/detail.php
@@ -2,6 +2,6 @@
     $description = $data['description'];
 ?>
 
-<div class="w-75 border border-1 border-dark p-5">
+<div class="w-75 border border-1 border-dark p-5 m-5">
     <h2>Listado de <?= $description ?></h2>
 </div>
